2b0057c71ec87a2d42bb83b46287b6abbb71a93f
milax1905 <axel.genest1905@gmaiL.com>
2025-09-04 16:34:45 +0200

    maj

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',              // <<< ESSENTIEL pour Electron (assets relatifs)
+  base: './',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   }
 });
