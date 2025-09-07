1d85a9e052dc6a323262cf4fde6d7d2978b5978d
milax1905 <axel.genest1905@gmaiL.com>
2025-09-07 11:10:01 +0200

    maj

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "./",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
-  }
+    chunkSizeWarningLimit: 1500, // augmente la limite d’avertissement (en kB)
+
+    // Optionnel : un peu de découpage pour réduire les gros chunks
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+  },
 });
